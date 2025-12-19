7185eb83e52e912c30d20a7554b4bba7a30648c4
Kain McDonald <sikeddemon@gmail.com>
2025-12-19 12:11:12 -0500

    new: Initial bot setup

---
+++ bot/utils/args.py
@@ -0,0 +1,25 @@
+# Standard Libraries
+import argparse
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    
+    # Token related arguments
+    parser.add_argument('-ts', '--tokenservice', help='The service name related to the token.', required=True)
+    parser.add_argument('-tn', '--tokenname', help='The name of the token stored.', required=True)
+
+    # Database related arguments
+    parser.add_argument('-db', '--database', help='The path to the SQLite database.', required=True)
+
+    # Logging related arguments
+    parser.add_argument('-ld', '--loggingdestination', help='The path where you would like to dump logs.', required=True)
+    parser.add_argument('-ll', '--logginglevel', help='The level of logging desired.', required=True)
+
+    # Testing related arguments
+    parser.add_argument('-tg', '--testingguild', help='The discord guild id for the test server.', required=True)
+
+    args = parser.parse_args()
+    return args
+    
+
